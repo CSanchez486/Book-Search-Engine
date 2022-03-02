9e47d3385dd17a05d06f105b20b364ec89a45420
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-02 13:49:38 -0500

    updated code that had deleted dependencies

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -26,14 +26,11 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
+      const { data } = await removeBook({
+        variables: { bookId },
+      });
+      console.log(data);
+     
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
