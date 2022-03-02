0d8225a3c32a07043002b380b1a4cba0a5257310
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-01 22:29:54 -0500

    linked imports to @apollo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
 
 function App() {
   return (
