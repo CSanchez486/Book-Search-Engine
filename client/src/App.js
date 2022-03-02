f9ca0e1166fc4bea2623d21c1f0dc454ca6c2347
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-01 22:51:57 -0500

    defined Apollo Client and created app function

--- client/src/App.js
+++ client/src/App.js
@@ -11,4 +11,42 @@ import {
   createHttpLink,
 } from "@apollo/client";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 
+// `App.js`: Create an Apollo Provider to make every request work with the Apollo Server.
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={SearchBooks} />
+            <Route exact path="/saved" component={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
