2873d24c5e44f8ca2bc35617c743c87aaca69b54
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-03-01 23:16:21 -0500

    created function to handle saving a book to our database.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -68,11 +68,19 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
+      console.log(data);
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
